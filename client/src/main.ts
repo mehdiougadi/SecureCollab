5feffda8dc9635b6d063ac3b1bc3ebbfb4a8a494
Mehdi <mehdiougadiofficial@gmail.com>
2025-06-29 23:02:40 -0400

    [SETUP] Mehdi: app module

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,6 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { platformBrowser } from '@angular/platform-browser';
+import { AppModule } from './app/modules/app.module';
 
-bootstrapApplication(App, appConfig)
+platformBrowser()
+  .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
