a60f741a6e5a72ffe0c0f3a2d1d7ab51fca66c30
Mehdi <mehdiougadiofficial@gmail.com>
2025-06-30 20:28:24 -0400

    [ADDED] Mehdi: auth form and auth wrapper components

--- client/src/app/features/auth/login/login.ts
+++ client/src/app/features/auth/login/login.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { AuthWrapper } from "@app/shared/components/auth-wrapper/auth-wrapper";
+import { AuthForm } from "../../../shared/components/auth-form/auth-form";
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [AuthWrapper, AuthForm],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
--- client/src/app/shared/components/auth-form/auth-form.ts
+++ client/src/app/shared/components/auth-form/auth-form.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-auth-form',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './auth-form.scss'
 })
 export class AuthForm {
-
+  @Input() title: string = '';
+  @Input() subtitle: string = '';
 }
